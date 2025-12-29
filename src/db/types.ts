b48c308ccbd9b826bd986c0e8c8b507879daf129
schaff <schaff.mathieu93@gmail.com>
2025-12-29 23:41:26 +0100

    db: delete index.test.ts and adding types.ts

---
+++ src/db/types.ts
@@ -0,0 +1,5 @@
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
+import type * as schema from "./schema";
+
+// Type générique pour n'importe quelle instance de DB
+export type DB = NodePgDatabase<typeof schema>;
