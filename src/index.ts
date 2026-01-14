224356579443fc44ac1f4930d4427d66949ddc5a
schaff <schaff.mathieu93@gmail.com>
2026-01-14 20:38:49 +0100

    refactor: changed authRoutes folder path in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 const port = Number(process.env.PORT ?? 3000);
 
 import { Hono } from "hono";
+// les routes
 import { healthRoute } from "./features/health/routes";
 import { habitsRoute } from "./features/habits/routes";
+import { profileRoute } from "./features/profile";
+
 import { db } from "./db/index";
-import { authRoutes } from "./features/auth/routes";
+import { authRoutes } from "./features/auth";
 import type { AppEnv } from "./app-env";
+// initialisation app hono
 const app = new Hono<AppEnv>();
+
 const appEnv: "development" | "production" =
   Bun.env.NODE_ENV === "production" ? "production" : "development";
+
 app.use("*", async (c, next) => {
   c.set("db", db);
   c.set("env", appEnv);
@@ -18,6 +24,7 @@ app.use("*", async (c, next) => {
 app.route("/auth", authRoutes);
 app.route("/", healthRoute);
 app.route("/habits", habitsRoute);
+app.route("/users", profileRoute);
 
 export default {
   port: port,
