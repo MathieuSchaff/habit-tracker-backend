276f72c77112a236d843c4f9611cc916b89a0c06
schaff <schaff.mathieu93@gmail.com>
2026-01-17 08:15:26 +0100

    refactor: more precise errorToStatus types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,4 +1,3 @@
-// ============================================================
 // Types génériques pour toutes les réponses API
 
 //  Types de base
@@ -50,30 +49,35 @@ export const HTTP_STATUS = {
 } as const;
 // cette function sert si on ajoute des HTTP_STATUS.
 // pas besoin de répéter type HttpStatus = ....
+// ça équivaut à type HttpStatus = 200 | 400 etc....
+// donc par exemple: le type HTTP_STATUS on cherche la clé ça fait type de HTTP_STATUS["OK"]
+// => ça nous renvoie les valeur en type
 export type HttpStatus = (typeof HTTP_STATUS)[keyof typeof HTTP_STATUS];
 
-// --- Codes d'erreur communs ---
-
-export type CommonErrorCode =
-  | "invalid_input"
-  | "not_found"
-  | "unauthorized"
-  | "forbidden"
-  | "server_error";
-
-//  Mapping erreurs -> status codes
-
-const baseErrorMapping: Record<string, HttpStatus> = {
+const baseErrorMapping = {
   invalid_input: HTTP_STATUS.BAD_REQUEST,
   not_found: HTTP_STATUS.NOT_FOUND,
   unauthorized: HTTP_STATUS.UNAUTHORIZED,
   forbidden: HTTP_STATUS.FORBIDDEN,
   server_error: HTTP_STATUS.INTERNAL_SERVER_ERROR,
-};
+  // satifies ajoute un typage supplémentaire.
+  // as const comme ça on dit pas que c'est juste des strings
+  // mais bien des clé de cet objet ( qu'on utuilise après)
+} as const satisfies Record<string, HttpStatus>;
 
-export const errorToStatus = (
-  error: string,
-  customMapping?: Record<string, HttpStatus>
+export type CommonErrorCode = keyof typeof baseErrorMapping;
+
+// convertir un code d'erruer en code HTTP
+//
+// Exclude<E, CommonErrorCode> retire de E les erreurs déjà gérées.
+//   Exemple avec login où E = "server_error" | "invalid_credentials" :
+//   "server_error" est dans CommonErrorCode → retiré
+//   "invalid_credentials" n'y est pas → gardé
+//   Donc customMapping doit contenir { invalid_credentials: 401 }
+export const errorToStatus = <E extends string>(
+  error: E,
+  // customMapping?: Record<string, HttpStatus>
+  customMapping: Record<Exclude<E, CommonErrorCode>, HttpStatus>
 ): HttpStatus => {
   const mapping = { ...baseErrorMapping, ...customMapping };
   return mapping[error] ?? HTTP_STATUS.INTERNAL_SERVER_ERROR;
