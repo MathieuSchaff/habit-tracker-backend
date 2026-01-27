cdedb8ba30ee09ff0a8aadc6909a6ba60c5675a4
schaff <schaff.mathieu93@gmail.com>
2026-01-28 00:00:31 +0100

    feat: created a reusubla spinner components

---
+++ frontend/src/component/Feedback/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import "./Spinner.css";
+import clsx from "clsx";
+
+type SpinnerProps = {
+  className?: string;
+};
+
+export const Spinner = ({ className }: SpinnerProps) => {
+  return <span className={clsx("spinner", className)} />;
+};
