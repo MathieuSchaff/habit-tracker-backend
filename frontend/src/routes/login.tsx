e41126c98785af4fa019d689caa67f3d968028a9
schaff <schaff.mathieu93@gmail.com>
2026-01-29 23:29:06 +0100

    feat: added login, and dashboard routes

---
+++ frontend/src/routes/login.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { LoginPage } from "../component/pages/LoginPage/LoginPage";
+
+export const Route = createFileRoute("/login")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="">
+      <LoginPage />
+    </div>
+  );
+}
