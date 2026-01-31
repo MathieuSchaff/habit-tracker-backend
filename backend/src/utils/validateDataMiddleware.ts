451aa659d068d2965a2371d69bb20c02e57f8c8e
schaff <schaff.mathieu93@gmail.com>
2026-01-31 07:36:38 +0100

    feat: creating a custom error for the zValidator middleware

--- backend/src/utils/validateDataMiddleware.ts
+++ backend/src/utils/validateDataMiddleware.ts
@@ -1,19 +1,21 @@
-// import { zValidator } from "@hono/zod-validator";
-// import z, { treeifyError } from "zod";
-// import { ApiError, err, HTTP_STATUS } from "../types/api";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { err, HTTP_STATUS, type ApiError } from "@habit-tracker/shared";
 
-// import { type ApiError, err, HTTP_STATUS } from "@habit-tracker/shared";
 export type ValidationTarget = "json" | "query" | "param" | "form" | "header";
-// export const validate = <T extends z.ZodSchema, E extends string>(
-//   target: ValidationTarget,
-//   schema: T,
 
-// ) =>
-//   zValidator(target, schema, (result, c) => {
-//     if (!result.success) {
-//       return c.json<ApiError<E>>(
-//         err("", treeifyError(result.error)),
-//         HTTP_STATUS.BAD_REQUEST
-//       );
-//     }
-//   });
+export const validate = <T extends z.ZodType>(
+  target: ValidationTarget,
+  schema: T
+) =>
+  zValidator(target, schema, (result, c) => {
+    if (!result.success) {
+      return c.json<ApiError<"invalid_input">>(
+        err("invalid_input", z.flattenError(result.error)),
+        HTTP_STATUS.BAD_REQUEST
+      );
+    }
+  });
+
+export const validateJson = <T extends z.ZodType>(schema: T) =>
+  validate("json", schema);
