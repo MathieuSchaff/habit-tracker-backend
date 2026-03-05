b89c329073a8d9d610aa4795810362f15817ba84
schaff <schaff.mathieu93@gmail.com>
2026-03-05 04:25:23 +0100

    feat: added the ingredients and product pages in the frontend.

---
+++ frontend/src/routes/ingredients/$slug.edit.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+import { IngredientEditPage } from '../../component/pages/Ingredient/IngredientEditpage'
+import { requireAuth } from '../../lib/auth/requireAuth'
+import { ingredientQueries } from '../../lib/queries/ingredients'
+
+export const Route = createFileRoute('/ingredients/$slug/edit')({
+  beforeLoad: async ({ context, location }) => {
+    await requireAuth({ queryClient: context.queryClient, pathname: location.pathname })
+  },
+
+  loader: ({ context, params }) =>
+    context.queryClient.ensureQueryData(ingredientQueries.bySlug(params.slug)),
+
+  component: IngredientEditPage,
+})
